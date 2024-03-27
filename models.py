5effcfc61c1de66079b6e072aaf9abb2396ac9c5
Elijah Mottey <elijahmottey5@gmail.com>
2024-03-27 14:56:52 -0700

    creating db and db_table

--- models.py
+++ models.py
@@ -2,5 +2,23 @@
 database section 
 it will take a feild
 
-"""
 
+"""
+#imported packages
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
+
+#create database,db
+db = SQLAlchemy()
+
+#creating a function to get uuid
+def getUuid():
+    return uuid4.hex()
+
+#creating database table containing user id , email and password
+
+class User(db.Model):
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=getUuid)
+    email = db.Column(db.String(345),nullable=False, unique=True)
+    password = db.Column(db.Text)
