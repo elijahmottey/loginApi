4e233365590172a31b46754e063ba5ab01cddbc3
Elijah Mottey <elijahmottey5@gmail.com>
2024-03-27 15:20:16 -0700

    configuration of the database

--- config.py
+++ config.py
@@ -1,6 +1,25 @@
+"""This is where all my configurations files goes to
+
+Keyword arguments:
+database
+mail 
+redis configuration
+
+"""
+from dotenv import load_dotenv
+import os
 
 
 
 class ApplicationConfig:
-    pass
+    #getting secret key
+    
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    
+    #configuring the database
+    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////user.db'
+    SQLALCHEMY_ECHO =True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 
--- models.py
+++ models.py
@@ -8,17 +8,17 @@ it will take a feild
 from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
 
-
+ 
 #create database,db
 db = SQLAlchemy()
 
 #creating a function to get uuid
 def getUuid():
-    return uuid4.hex()
+    return uuid4().hex
 
 #creating database table containing user id , email and password
 
 class User(db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=getUuid)
     email = db.Column(db.String(345),nullable=False, unique=True)
-    password = db.Column(db.Text)
+    password = db.Column(db.Text, default=getUuid ,nullable=False)
